3784b2cf3ea11886f209ee2cb9a79dcd7ad92dde
Albert Portnoy <albert@albertp.dev>
2021-10-16 12:53:00 -0500

    Change valid URL checking

--- script.js
+++ script.js
@@ -141,16 +141,13 @@ async function checkJSON(data) {
 function isValidURL(url) {
 	if (!url) return true;
 	if (typeof url !== 'string') return false;
-	const pattern = new RegExp(
-		'^(https?:\\/\\/)?' + // protocol
-			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-			'(\\#[-a-z\\d_]*)?$',
-		'i',
-	); // fragment locator
-	return !!pattern.test(url);
+	try {
+		let urlobj = new URL(url);
+		if (!['http:', 'https:'].includes(urlobj.protocol)) return false;
+		return true;
+	} catch (e) {
+		return false;
+	}
 }
 
 /**
